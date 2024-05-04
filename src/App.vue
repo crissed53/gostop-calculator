7bad6bbbafa11a2552a2433d8b31a84cb37b419b
crissed <crissed@MacBook-Pro-91.local>
2024-05-05 00:32:32 +0900

    update

--- src/App.vue
+++ src/App.vue
@@ -6,11 +6,16 @@ import { ref } from "vue";
 
 // const { userInfos, addUser, addVal } = useCounterStore();
 const userInfos = useStorage("userInfos", [
-  { userName: "조찬숙", count: 0 },
-  { userName: "정연우", count: 0 },
+  { userName: "조찬숙", count: 0, multiplier: 1, selected: true },
+  { userName: "정연우", count: 0, multiplier: 1, selected: true },
 ]);
 const addUser = (userName) => {
-  userInfos.value.push({ userName: userName, count: 0 });
+  userInfos.value.push({
+    userName: userName,
+    count: 0,
+    multiplier: 1,
+    selected: true,
+  });
 };
 const deleteUser = (userName) => {
   let index;
@@ -29,12 +34,24 @@ const deleteUser = (userName) => {
   // }
   // userInfos.value = result;
 };
-const addVal = (userName, count) => {
+const addVal = (selectedUserInfo, count) => {
+  if (!selectedUserInfo.selected) {
+    return;
+  }
+  let totalMultiplier = 0;
+  userInfos.value.forEach((userInfo) => {
+    if (userInfo.selected) {
+      totalMultiplier = totalMultiplier + userInfo.multiplier;
+    }
+  });
+  console.log(totalMultiplier);
   for (const userInfo of userInfos.value) {
-    if (userInfo.userName === userName) {
-      userInfo.count = userInfo.count + (userInfos.value.length - 1) * count;
+    if (!userInfo.selected) continue;
+    if (userInfo.userName === selectedUserInfo.userName) {
+      userInfo.count =
+        userInfo.count + (totalMultiplier - userInfo.multiplier) * count;
     } else {
-      userInfo.count = userInfo.count - count;
+      userInfo.count = userInfo.count - count * userInfo.multiplier;
     }
   }
 };
@@ -43,56 +60,69 @@ const registerMember = () => {
   addUser(inputValue.value);
   inputValue.value = "";
 };
+console.log(userInfos.value);
 </script>
 
 <template>
   <div>
     <div v-for="userInfo in userInfos">
       <div class="flex">
-        <div class="h-16 border border-md text-2xl">
+        <input
+          type="checkbox"
+          class="h-16 w-16"
+          v-model="userInfo.selected"
+        />
+        <button class="h-16 border border-md text-xl cursor-default p-4">
           {{ userInfo.userName }}
-        </div>
-        <div class="w-16 h-16 border rounded-md text-2xl text-center">
+        </button>
+        <button class="w-16 h-16 border rounded-md text-xl cursor-default p-4">
           {{ userInfo.count }}
+        </button>
+        <div class="h-16 border rounded-md text-xl cursor-default p-4 flex">
+          x<input
+            class="w-3"
+            type="text"
+            v-model.number="userInfo.multiplier"
+          />
         </div>
         <button
           class="w-16 h-16 border rounded-md"
-          @click="addVal(userInfo.userName, 1)"
+          @click="addVal(userInfo, 1)"
         >
           +
         </button>
         <button
           class="w-16 h-16 border rounded-md"
-          @click="addVal(userInfo.userName, 5)"
+          @click="addVal(userInfo, 5)"
         >
           5+
         </button>
         <button
           class="w-16 h-16 border rounded-md"
-          @click="addVal(userInfo.userName, 10)"
+          @click="addVal(userInfo, 10)"
         >
           10+
         </button>
         <button
           class="w-16 h-16 border rounded-md"
-          @click="addVal(userInfo.userName, -1)"
+          @click="addVal(userInfo, -1)"
         >
           -
         </button>
         <button
           class="w-16 h-16 border rounded-md"
-          @click="addVal(userInfo.userName, -5)"
+          @click="addVal(userInfo, -5)"
         >
           5-
         </button>
         <button
           class="w-16 h-16 border rounded-md"
-          @click="addVal(userInfo.userName, -10)"
+          @click="addVal(userInfo, -10)"
         >
           10-
         </button>
         <button
-          class="w-16 h-16 border rounded-md"
+          class="w-16 h-16 border rounded-md bg-red-500 text-white"
           @click="deleteUser(userInfo.userName)"
         >
           X
@@ -101,13 +131,12 @@ const registerMember = () => {
     </div>
     <div>
       <input
-        class="h-8 w-64 border"
+        class="h-16 w-64 border px-4 text-xl"
         type="text"
-        placeholder="add member"
         v-model="inputValue"
       />
       <button
-        class="h-16 border rounded-md"
+        class="h-16 border rounded-md p-4 text-xl bg-green-300 text-gray-500"
         @click="registerMember"
       >
         AddMember
